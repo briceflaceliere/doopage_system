71ed12f76c8e95446a5237bde5c38eec337d2f82
Ugurrms <contact@b-flaceliere.fr>
2013-10-13 18:42:32 +0200

    Signed-off-by: Ugurrms <contact@b-flaceliere.fr>

---
+++ Core/Lib/LibraryBrick.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DoopageFramework\Core;
+
+/**
+ * Description de AppBrick
+ *
+ * @author Flaceliere Brice <contact@b-flaceliere.fr>
+ */
+class Lib_AppBrick {
+    //put your code here
+}
+
+?>
